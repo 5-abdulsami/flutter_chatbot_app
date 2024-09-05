ad565d7b9f987326e7337ed797bdc683fb85643d
5-abdulsami <5abdulsami2004@gmail.com>
2024-09-05 23:25:54 +0500

    added methods in chatProvider, added chatMessagesBox in constants

--- lib/provider/chat_provider.dart
+++ lib/provider/chat_provider.dart
@@ -1,5 +1,7 @@
+import 'dart:core';
+import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
+
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_chatbot_app/api/api_service.dart';
 import 'package:flutter_chatbot_app/constants.dart';
@@ -8,7 +10,6 @@ import 'package:flutter_chatbot_app/hive/settings.dart';
 import 'package:flutter_chatbot_app/hive/user_model.dart';
 import 'package:flutter_chatbot_app/model/message_model.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
@@ -24,7 +25,7 @@ class ChatProvider extends ChangeNotifier {
 
   String _currentChatId = "";
 
-  GenerativeModel? _gModel;
+  GenerativeModel? _model;
 
   GenerativeModel? _textModel;
 
@@ -34,12 +35,92 @@ class ChatProvider extends ChangeNotifier {
 
   bool _isLoading = false;
 
+  // setters
+
+  // set inChatMessages
+  Future<void> setInChatMessages({required String chatId}) async {
+    // get messages from Hive db
+    final messagesFromDb = await loadMessagesFromDb(chatId: chatId);
+
+    for (var message in messagesFromDb) {
+      if (_inChatMessages.contains(message)) {
+        log("this message already exists");
+        continue;
+      }
+      _inChatMessages.add(message);
+    }
+    notifyListeners();
+  }
+
+  // load messages from db
+  Future<List<Message>> loadMessagesFromDb({required String chatId}) async {
+    // open the box of this chatId
+    await Hive.openBox("${Constants.chatMessagesBox}$chatId");
+
+    final messageBox = Hive.box("${Constants.chatMessagesBox}$chatId");
+    final newData = messageBox.keys.map((key) {
+      final message = messageBox.get(key);
+      final messageData = Message.fromMap(Map<String, dynamic>.from(message));
+
+      return messageData;
+    }).toList();
+    notifyListeners();
+    return newData;
+  }
+
+  // set image files list
+  void setImageFilesList({required List<XFile> imagesList}) {
+    _imageFilesList = imagesList;
+    notifyListeners();
+  }
+
+  // set the current model
+  String setCurrentModel({required String newModel}) {
+    _modelType = newModel;
+    notifyListeners();
+    return newModel;
+  }
+
+  // function to set the model based on the bool : isTextOnly
+  Future<void> setModel({required bool isTextOnly}) async {
+    if (isTextOnly) {
+      _model = _textModel ??
+          GenerativeModel(
+              model: setCurrentModel(newModel: "gemini-pro"),
+              apiKey: ApiService.API_KEY);
+    } else {
+      _model = _visionModel ??
+          GenerativeModel(
+              model: setCurrentModel(newModel: "gemini-pro-vision"),
+              apiKey: ApiService.API_KEY);
+    }
+    notifyListeners();
+  }
+
+  // set current page index
+  void setCurrentIndex({required int newIndex}) {
+    _currentIndex = newIndex;
+    notifyListeners();
+  }
+
+  // set current chatId
+  void setChatid({required String newChatId}) {
+    _currentChatId = newChatId;
+    notifyListeners();
+  }
+
+  // set loading
+  void setLoading({required bool newLoading}) {
+    _isLoading = newLoading;
+    notifyListeners();
+  }
+
   List<dynamic> get inChatMessages => _inChatMessages;
   PageController get pageController => _pageController;
   List<XFile> get imageFilesList => _imageFilesList!;
   int get currentIndex => _currentIndex;
   String get currentChatId => _currentChatId;
-  GenerativeModel get gModel => _gModel!;
+  GenerativeModel get model => _model!;
   GenerativeModel get textModel => _textModel!;
   GenerativeModel get visionModel => _visionModel!;
   String get modelType => _modelType;
